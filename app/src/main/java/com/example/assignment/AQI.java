b3a582ace69ffc59b8a42010e3633343924409ef
Ditto226 <aqilsuid@gmail.com>
2024-01-12 17:00:01 +0800

    commit

--- app/src/main/java/com/example/assignment/AQI.java
+++ app/src/main/java/com/example/assignment/AQI.java
@@ -182,10 +182,6 @@ public class AQI extends AppCompatActivity implements IBaseGpsListener{
 
                                     for (int i = 0; i < listdate.size(); i++) {
                                             count++;
-//                                            Calendar cal1 = Calendar.getInstance();
-//                                            cal1.setTime(date);
-//                                            Calendar cal2 = Calendar.getInstance();
-//                                            cal2.setTime(date2);
                                             if(i<listdate.size()-1){
                                                 Date date = sdf.parse(listdate.get(i));
                                                 Date date2 = sdf.parse(listdate.get(i + 1));
@@ -313,11 +309,11 @@ public class AQI extends AppCompatActivity implements IBaseGpsListener{
         tmrw2.setText(tmrw3S);
         tmrw3.setText(tmrw4S);
     }
-    public static boolean isSameDay(Calendar cal1, Calendar cal2) {
-        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
-                cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
-    }
+//    public static boolean isSameDay(Calendar cal1, Calendar cal2) {
+//        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+//                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
+//                cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
+//    }
 
 //    public String firstDate() {
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
